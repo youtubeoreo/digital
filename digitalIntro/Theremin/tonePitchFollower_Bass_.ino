bd71b094472f2c137a18fdafa546732989404f67
gskielian <greg@techlabeducation.com>
2013-06-20 11:19:20 -0600

    Update tonePitchFollower_Bass_.ino

--- digitalIntro/Theremin/tonePitchFollower_Bass_.ino
+++ digitalIntro/Theremin/tonePitchFollower_Bass_.ino
@@ -6,6 +6,13 @@ void setup() {
 void loop() {
   
   int sensorReading = analogRead(A0);
+  int midPoint = 350;
+  if (sensorReading > midPoint)
+  Serial.println("Everythings chill; Party on!");
+  }
+  else
+  {
+  Serial.println("Alert -- release the drones!");
  
   Serial.println(sensorReading) ;
   delay(1);       
