25b4ff20f02568a9c2a38a3503106d1ccb0f82f5
gskielian <greg@techlabeducation.com>
2013-06-20 11:08:06 -0600

    Update tonePitchFollow_Treble.ino

--- digitalIntro/Theremin/tonePitchFollow_Treble.ino
+++ digitalIntro/Theremin/tonePitchFollow_Treble.ino
@@ -5,10 +5,5 @@ Serial.begin(9600);
 void loop() {
   int sensorReading = analogRead(A0);
   Serial.println(sensorReading);
-  
-  int thisPitch = map(sensorReading, 30, 130, 50, 100);
-
-  // play the pitch:
-  tone(9, thisPitch, 10);
-  delay(1);        // delay in between reads for stability
+  delay(1);
 }
